5a827f6e661d380a8427f4635dff65b6a80df458
MackHartley <mhartley@macalester.edu>
2016-02-24 00:04:28 -0600

    Refactored the Student and Course classes!

--- src/registrar/Course.java
+++ src/registrar/Course.java
@@ -10,74 +10,94 @@ import java.util.Set;
  */
 public class Course {
 
-    private Set<Student> enrolledIn;
-    private List<Student> waitlist;
-    private String number;
-    private String name;
-    private int limit;
+    private Set<Student> studentsEnrolledInClass;//Students actually in the class
+    private List<Student> studentsInWaitlist;//Students on a waitlist for the class
+    private String courseIdNumber;
+    private String courseIdName;
+    private int enrolementLimit;//Max amount of students a class can hold
 
     public Course(){
-        enrolledIn = new HashSet<>();
-        waitlist = new ArrayList<>();
-        limit = 16;
+        studentsEnrolledInClass = new HashSet<>();
+        studentsInWaitlist = new ArrayList<>();
+        enrolementLimit = 16;
+    }//Course object
+
+    /**
+     * Enrolls a given student into this course
+     * @param student a student to be enrolled into the course
+     * @return whether or not the student is in the class, or can get in the class
+     */
+    public boolean enrollIn(Student student){
+        if (studentsEnrolledInClass.contains(student)){
+            return true;
+        }
+        else if (studentsEnrolledInClass.size() >= enrolementLimit){//If the class is at or over capacity
+            if (studentsInWaitlist.contains(student)){//If the student is already in the waitlist
+                return false;
+            }
+            else{
+                studentsInWaitlist.add(student);//add student to waitlist
+                return false;
+            }
+        }
+        else{
+            studentsEnrolledInClass.add(student);
+            return true;
+        }
+
     }
 
-    public void setCatalogNumber(String number){
-        this.number = number;
+    /**
+     * This method drops a chosen student
+     * @param student the selected student to be dropped
+     */
+    public void dropStudent(Student student){
+        if (studentsEnrolledInClass.contains(student)) {//If the student is in the class
+            studentsEnrolledInClass.remove(student);//remove the student
+            if (studentsInWaitlist.size() > 0) {//If there is a waitlist
+                Student nextToEnroll = studentsInWaitlist.remove(0);//Enrolle the next student in the class
+                studentsEnrolledInClass.add(nextToEnroll);
+                nextToEnroll.coursesEnrolledIn.add(this);
+            }
+        }
+        else{//removes a student from a class if they are in the waitlist
+            studentsInWaitlist.remove(student);
+        }
     }
 
-    public void setTitle(String title){
-        this.name = title;
-    }
-
-    public int getEnrollmentLimit(){
-        return limit;
-    }
-
+    /**
+     * Sets the enrollment limit for a class
+     * @param limit an int which signifies how many students the class can hold
+     * @return returns true or false if the class is empty. True is empty
+     */
     public boolean setEnrollmentLimit(int limit){
-        //If students are enrolled you can't change the limit
-        if (enrolledIn.size() == 0){
-            this.limit = limit;
+        //If students are enrolled you can't change the enrolementLimit
+        if (studentsEnrolledInClass.size() == 0){
+            this.enrolementLimit = limit;
             return true;
         }
         return false;
     }
 
+    //Getters and Setters
+    public int getEnrollmentLimit(){
+        return enrolementLimit;
+    }
+
     public Set<Student> getStudents(){
-        return enrolledIn;
+        return studentsEnrolledInClass;
     }
 
     public List<Student> getWaitList(){
-        return waitlist;
+        return studentsInWaitlist;
     }
 
-    public boolean enrollIn(Student s){
-        if (enrolledIn.contains(s)){
-            return true;
-        }
-        if (enrolledIn.size() >= limit){
-            if (waitlist.contains(s)){
-                return false;
-            }
-            waitlist.add(s);
-            return false;
-        }
-        enrolledIn.add(s);
-        return true;
+    public void setCatalogNumber(String number){
+        this.courseIdNumber = number;
     }
 
-    public void dropStudent(Student s){
-        if (enrolledIn.contains(s)) {
-            enrolledIn.remove(s);
-            if (waitlist.size() > 0) {
-                Student toEnroll = waitlist.remove(0);
-                enrolledIn.add(toEnroll);
-                toEnroll.enrolledIn.add(this);
-            }
-        }
-        else if (waitlist.contains(s)){
-            waitlist.remove(s);
-        }
+    public void setTitle(String title){
+        this.courseIdName = title;
     }
 
 }
