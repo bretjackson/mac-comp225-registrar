4cdbdbca875eaa47c0af404f0d8f098c49e5f474
Rae Hohle <rhohle@macalester.edu>
2016-02-23 22:58:40 -0600

    Finished assignment. Added 2 tests.

--- src/registrar/Student.java
+++ src/registrar/Student.java
@@ -1,43 +1,61 @@
 package registrar;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
- * Created by bjackson on 2/21/2016.
+ * Created by bjackson on 2/21/2016. Edited by Rae.
+ */
+
+/**
+ * This Student class allows each student to know it's name and courses it is enrolled in.
+ * A student can drop a course (dropCourse) or attempt to enroll in a course (enrollIn).
  */
 public class Student {
 
     public String name;
-    public Set<Course> enrolledIn;
+    public Set<Course> classSchedule;
 
     public Student(){
-        enrolledIn = new HashSet<>();
+        classSchedule = new HashSet<>();
     }
 
     public void setName(String name){
         this.name = name;
     }
 
-    public Set<Course> getCourses(){
-        return enrolledIn;
+    public Set<Course> getSchedule(){
+        return classSchedule;
     }
 
-    public boolean enrollIn(Course c){
-        if(c.enrollIn(this)) {
-            enrolledIn.add(c);
+    /**
+     * Allows a student to drop a course. Removes the course from student's class schedule.
+     * Removes student from course roster.
+     * @param course student wants to drop
+     */
+    public void dropCourse(Course course){
+        classSchedule.remove(course);
+        course.dropStudent(this);
+    }
+
+    /**
+     * Enrolls a student in a course. Returns result of adding student in boolean.
+     * @param course student wants to add
+     * @return true if student is enrolled in the course and false if student must be added to wait list.
+     */
+    public boolean enrollIn(Course course){
+        if (course.getRoster().contains(this)) {
             return true;
         }
-        else {
+        else if (course.getRoster().size() < course.getEnrollmentLimit()) {
+            course.getRoster().add(this);
+            classSchedule.add(course);
+            return true;
+        }
+        else if (course.getRoster().size() >= course.getEnrollmentLimit() && !course.getWaitList().contains(this)) {
+            course.getWaitList().add(this);
             return false;
         }
-    }
-
-    public void drop(Course c){
-        if (enrolledIn.contains(c)) {
-            enrolledIn.remove(c);
-        }
-        c.dropStudent(this);
+        return false;
     }
 }
