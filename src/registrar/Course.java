7723f302dfa3cdda57481a3bd4bab1a2f36063f8
Qinxi Wang <qwang@macalester.edu>
2016-02-23 23:45:44 -0600

    refactoring

--- src/registrar/Course.java
+++ src/registrar/Course.java
@@ -16,68 +16,85 @@ public class Course {
     private String name;
     private int limit;
 
-    public Course(){
+    public Course() {
         enrolledIn = new HashSet<>();
         waitlist = new ArrayList<>();
         limit = 16;
     }
 
-    public void setCatalogNumber(String number){
+    public void setCatalogNumber(String number) {
         this.number = number;
     }
 
-    public void setTitle(String title){
+    public void setTitle(String title) {
         this.name = title;
     }
 
-    public int getEnrollmentLimit(){
+    public int getEnrollmentLimit() {
         return limit;
     }
 
-    public boolean setEnrollmentLimit(int limit){
-        //If students are enrolled you can't change the limit
-        if (enrolledIn.size() == 0){
+    public boolean setEnrollmentLimit(int limit) {
+        if (enrolledIn.isEmpty()) {
             this.limit = limit;
             return true;
         }
         return false;
     }
 
-    public Set<Student> getStudents(){
+    public Set<Student> getStudents() {
         return enrolledIn;
     }
 
-    public List<Student> getWaitList(){
+    public List<Student> getWaitList() {
         return waitlist;
     }
 
-    public boolean enrollIn(Student s){
-        if (enrolledIn.contains(s)){
+    public boolean enrollIn(Student s) {
+        if (enrollStatus(s)=="enrolled") {
             return true;
         }
-        if (enrolledIn.size() >= limit){
-            if (waitlist.contains(s)){
-                return false;
-            }
-            waitlist.add(s);
+        if (enrollStatus(s)=="waitlisted"){
             return false;
         }
-        enrolledIn.add(s);
+        if (enrolledIn.size() >= limit) {
+            addStudent(s);
+            return false;
+        }
+        addStudent(s);
         return true;
     }
 
-    public void dropStudent(Student s){
+    public String enrollStatus(Student s) {
         if (enrolledIn.contains(s)) {
-            enrolledIn.remove(s);
-            if (waitlist.size() > 0) {
-                Student toEnroll = waitlist.remove(0);
-                enrolledIn.add(toEnroll);
-                toEnroll.enrolledIn.add(this);
-            }
-        }
-        else if (waitlist.contains(s)){
-            waitlist.remove(s);
-        }
+            return "enrolled";
+        } else if (waitlist.contains(s)) {
+            return "waitlisted";
+        } else return "notenrolled";
     }
 
+    public void addStudent(Student s) {
+        if (enrolledIn.size() >= limit) {
+            waitlist.add(s);
+        }
+        else enrolledIn.add(s);
+    }
+
+    public void removeStudentfromWaitlist() {
+        Student toEnroll = waitlist.remove(0);
+        enrolledIn.add(toEnroll);
+        toEnroll.enrollIn(this);
+    }
+
+    public void dropStudent(Student s) {
+        if(enrollStatus(s) == "enrolled"){
+            enrolledIn.remove(s);
+            if (waitlist.size() > 0) {
+                removeStudentfromWaitlist();
+            }
+        }
+        else waitlist.remove(s);
+        }
 }
+
+
