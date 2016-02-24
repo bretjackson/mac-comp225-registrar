bcbc0c07ccc6493780976e0fcfc6f57dc5d9897a
bgithinji <bgithinj@macalester.edu>
2016-02-24 02:47:02 -0600

    Added javadoc comments

--- src/registrar/Course.java
+++ src/registrar/Course.java
@@ -16,24 +16,44 @@ public class Course {
     private String name;
     private int limit;
 
+    /**
+     * Constructor
+     */
     public Course(){
         enrolledIn = new HashSet<>();
         waitlist = new ArrayList<>();
         limit = 16;
     }
 
+    /**
+     * Sets the course catalog number
+     * @param number
+     */
     public void setCatalogNumber(String number){
         this.number = number;
     }
 
+    /**
+     * Sets the course title
+     * @param title
+     */
     public void setTitle(String title){
         this.name = title;
     }
 
+    /**
+     * Gets the course's enrollment limit
+     * @return
+     */
     public int getEnrollmentLimit(){
         return limit;
     }
 
+    /**
+     * Sets course's enrollment limit
+     * @param limit
+     * @return
+     */
     public boolean setEnrollmentLimit(int limit){
         //If students are enrolled you can't change the limit
         if (enrolledIn.size() == 0){
@@ -43,14 +63,27 @@ public class Course {
         return false;
     }
 
+    /**
+     * Gets a set of students in course
+     * @return set of students
+     */
     public Set<Student> getStudents(){
         return enrolledIn;
     }
 
+    /**
+     * Gets the list of students on wait list for course
+     * @return list of students
+     */
     public List<Student> getWaitList(){
         return waitlist;
     }
 
+    /**
+     * Checks if student is enrolled
+     * @param s student
+     * @return
+     */
     public boolean enrollIn(Student s){
         if (enrolledIn.contains(s)){
             return true;
@@ -66,6 +99,10 @@ public class Course {
         return true;
     }
 
+    /**
+     * Removes student from enrollment list
+     * @param s student
+     */
     public void dropStudent(Student s){
         if (enrolledIn.contains(s)) {
             enrolledIn.remove(s);
--- src/registrar/Student.java
+++ src/registrar/Student.java
@@ -12,18 +12,34 @@ public class Student {
     public String name;
     public Set<Course> enrolledIn;
 
+    /**
+     * Constructor
+     */
     public Student(){
         enrolledIn = new HashSet<>();
     }
 
+    /**
+     * Set student's name
+     * @param name
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * Sets course for student
+     * @return
+     */
     public Set<Course> getCourses(){
         return enrolledIn;
     }
 
+    /**
+     * Adds student to class list
+     * @param c
+     * @return
+     */
     public boolean enrollIn(Course c){
         if(c.enrollIn(this)) {
             enrolledIn.add(c);
@@ -34,6 +50,10 @@ public class Student {
         }
     }
 
+    /**
+     * Removes student from course
+     * @param c
+     */
     public void drop(Course c){
         if (enrolledIn.contains(c)) {
             enrolledIn.remove(c);
