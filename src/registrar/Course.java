73b567fc4c67f9124499cb50175d33401331d292
angelicalin <angelicalin7@gmail.com>
2016-02-28 14:00:24 -0600

    I finished implementing unlimited enroll feature. However, the Test cannot run. It's weird. There is no red signs anywhere in the test class.

--- src/registrar/Course.java
+++ src/registrar/Course.java
@@ -1,12 +1,6 @@
 package registrar;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by bjackson on 2/21/2016.
@@ -18,6 +12,7 @@ public class Course {
     private String catalogNumber;
     private String title;
     private int enrollmentLimit = 16;
+    private Boolean unlimitedEnroll = true;
 
     public String getCatalogNumber() {
         return catalogNumber;
@@ -44,15 +39,15 @@ public class Course {
     }
 
     public boolean setEnrollmentLimit(int limit) {
-        if (limit < 0) {
+        if (limit<roster.size()) {
             throw new IllegalArgumentException("course cannot have negative enrollment limit: " + limit);
         }
 
         //If students are enrolled you can't change the limit
-        if (!roster.isEmpty()) {
-            return false;   // Consider making this IllegalStateException instead of boolean return val
-        }
-
+//        if (!roster.isEmpty()) {
+//            return false;   // Consider making this IllegalStateException instead of boolean return val
+//        }
+        unlimitedEnroll = false;
         this.enrollmentLimit = limit;
         return true;
     }
@@ -78,6 +73,9 @@ public class Course {
     }
 
     public boolean isFull() {
+        if (unlimitedEnroll){
+            return false;
+        }
         return roster.size() >= enrollmentLimit;
     }
 
@@ -98,6 +96,9 @@ public class Course {
         if (roster.remove(student)) {
             enrollNextFromWaitlist();
         }
+    }
+    public void removeEnrollmentLimit(){
+        unlimitedEnroll = true;
     }
 
     @Override
